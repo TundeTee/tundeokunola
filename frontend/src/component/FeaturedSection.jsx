5b3cc3d6e49267bf6b1e23c5f679de759e049703
Tunde <olaseni2019@gmail.com>
2025-11-19 10:48:12 +0100

    finally

--- frontend/src/component/FeaturedSection.jsx
+++ frontend/src/component/FeaturedSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FeaturedSection = () => {
   return (
-    <section className='py-16 px-4 bg-white relative top-20'>
+    <section className='py-16 px-4 bg-white relative top-4'>
     <div className="container mx-auto grid grid-cols-1 lg:grid-cols-4 gap-8 text-center ">
     <div className="flex flex-col items-center bg-gray-300 h-full w-full pb-5">
         <h1 className='font-bold text-2xl italic p-2'>01.</h1>
